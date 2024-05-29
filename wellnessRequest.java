da6bf60b9e8f7626e2184b605986e525076e44d6
tahir-05 <143780390+tahir-05@users.noreply.github.com>
2024-05-29 17:50:00 +0530

    Update wellnessRequest.java

--- wellnessRequest.java
+++ wellnessRequest.java
@@ -6,13 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class wellnessRequest {
     public static void main(String[] args) throws InterruptedException {
         // Set the path to the ChromeDriver executable
-        System.setProperty("webdriver.chrome.driver", "C:\\SeleniumTest\\CrmTest\\src\\drivers\\chromedriver-win64\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\pinc\\PincNetwork\\src\\drivers\\chromedriver.exe");
         // Initialize ChromeDriver
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://network.staypinc.com/login");
 
         positiveLogin(driver);
+        logout(driver);
+        login(driver, "approval.manager@pincinsure.com", "ApprovalManagerPassword");
+
       
 
     }
@@ -53,17 +56,100 @@ public class wellnessRequest {
         
         WebElement wellnesRequest = driver.findElement(By.linkText("Wellness Requests"));
         wellnesRequest.click();
+        Thread.sleep(2000);
 
-       // (By.cssSelector("li.nav-item > button.btn.btn-primary.btn-md.px-1.font-small-3.shadow.round"));
-       //"button.btn.btn-primary.btn-md.px-1.font-small-3.shadow.round")
-      
-       WebElement raiseNewRequestButton =driver.findElement(By.cssSelector("li.nav-item > button.btn.btn-primary.btn-md.px-1.font-small-3.shadow.round"));
-        raiseNewRequestButton.click();
-      
+        WebElement raiseRequestButton = driver.findElement(By.xpath("//button[@class='btn btn-primary btn-md px-1 font-small-3 shadow round']"));
+         raiseRequestButton.click();
 
-       
+         WebElement clientType = driver.findElement(By.cssSelector("span.e-input-group-icon.e-ddl-icon.e-search-icon"));
+         clientType.click();
+         Thread.sleep(1000);
  
+         WebElement clienttypeselect = driver.findElement(By.cssSelector(".col-12.col-md-6.col-lg-6 .e-input-group"));
+         clienttypeselect.sendKeys("Existing Client"); 
+         Thread.sleep(2000);
+ 
+         WebElement clientNameselect = driver.findElement(By.cssSelector(".col-12.col-md-6.col-lg-6:nth-child(2) .e-input-group"));
+         clientNameselect.sendKeys("ABCC");
+         Thread.sleep(2000);
+
+         WebElement location = driver.findElement(By.id("address"));
+         location.sendKeys("mumbai thane");
+
+         WebElement Date = driver.findElement(By.className("dx-dropdowneditor-icon"));
+         Date.click();
+ 
+         WebElement selectedCalCell = driver.findElement(By.cssSelector("td.dx-calendar-today"));
+         selectedCalCell.click(); 
+         Thread.sleep(2000);
+ 
+         //WebElement remark = driver.findElement(By.id("remarks"));
+        // remark.sendKeys("wellness Testing");
+
+         WebElement submitButton = driver.findElement(By.id("save_wellness_management"));
+         submitButton.click();
+         Thread.sleep(2000);
+
         
     }
+
+    public static void logout(WebDriver driver) throws InterruptedException {
+        WebElement profileMenu = driver.findElement(By.linkText("My Profile")); 
+        profileMenu.click();
+        Thread.sleep(2000);
+
+        WebElement userDropdown = driver.findElement(By.cssSelector("li.dropdown-user.nav-item .dropdown-toggle.nav-link"));
+        userDropdown.click();
+
+        WebElement logoutButton = driver.findElement(By.cssSelector(".dropdown-menu a:nth-child(2)")); 
+        logoutButton.click();
+        Thread.sleep(1000);
+
+
+
+
     
+}
+public static void login(WebDriver driver, String username, String password) throws InterruptedException {
+
+    WebElement EmailAddressField = driver.findElement(By.id("email"));
+    EmailAddressField.sendKeys("rahul.chauhan@dev.pincinsure.com");
+
+    WebElement passwordField = driver.findElement(By.id("password"));
+    passwordField.sendKeys("Pincdev@123");
+   
+    WebElement loginButton = driver.findElement(By.id("login"));
+    loginButton.click();
+    Thread.sleep(2000);
+
+    WebElement WellnesssManagement = driver.findElement(By.linkText("Wellness Management"));
+    WellnesssManagement.click();
+    Thread.sleep(1000);
+
+    WebElement requestApproval = driver.findElement(By.linkText("Request Approvals"));
+    requestApproval.click();
+    Thread.sleep(2000);
+
+    WebElement eyeButton2 = driver.findElement(By.xpath("//td[@class='dx-command-edit']//button[2]"));
+    eyeButton2.click();
+    Thread.sleep(1000);
+
+    WebElement status = driver.findElement(By.id("status"));
+    status.sendKeys("Approve");
+
+    WebElement reasonApproval = driver.findElement(By.id("reason"));
+    reasonApproval.sendKeys("Testing Approve");
+
+    WebElement SubmitApproval = driver.findElement(By.id("edit_wellness_approval"));
+    SubmitApproval.click();
+
+    logout(driver);
+    positiveLogin(driver);
+    login(driver, "approval.manager@pincinsure.com", "ApprovalManagerPassword");
+
+
+
+
+}
+
 }
